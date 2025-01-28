e69e638dd761ec6711422c850d093dfb3b9da42f
Alex Dedyaev <154517065+AlexDedyaev@users.noreply.github.com>
2025-01-28 10:06:59 +0300

    Fix#1

--- src/BmiService.java
+++ src/BmiService.java
@@ -1,5 +1,5 @@
 public class BmiService {
-    public int calculate(float weight, float height) {
+    public int Calculate(float weight, float height) {
         float bmi = weight / (height * height);
         return (int) bmi;
 
